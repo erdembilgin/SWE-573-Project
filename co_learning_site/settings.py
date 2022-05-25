46a42a1de16ab6cfe96b0caa59bfe68eb4272dc2
erdembilgin <hasanerdembilgin@gmail.com>
2022-05-25 10:42:15 +0000

    commit from ubuntu instance AWS

--- co_learning_site/settings.py
+++ co_learning_site/settings.py
@@ -87,12 +87,12 @@ DATABASES = {
     #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
     'default': {
-        'ENGINE': config("ENGINE"),
-        'NAME': config("NAME"),
-        'USER': config("USER"),
-        'PASSWORD': config("PASSWORD"),
-        'HOST': config("HOST"),
-        'PORT': config("PORT"),
+        'ENGINE': "django.db.backends.postgresql_psycopg2",
+        'NAME': "django_db",
+        'USER': "python_dev",
+        'PASSWORD': "aqwe123",
+        'HOST': "localhost",
+        'PORT': 5432,
     },
     # 'default': {
     #     'ENGINE': 'django.db.backends.postgresql',
