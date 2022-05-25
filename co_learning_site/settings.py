cbb94b0e1d085a18567055cf3faae16a19c2499f
erdembilgin <hasanerdembilgin@gmail.com>
2022-05-25 11:15:00 +0500

    database settings changed for deployment

--- co_learning_site/settings.py
+++ co_learning_site/settings.py
@@ -86,22 +86,22 @@ DATABASES = {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
-    # 'default': {
-    #     'ENGINE': config("ENGINE"),
-    #     'NAME': config("NAME"),
-    #     'USER': config("USER"),
-    #     'PASSWORD': config("PASSWORD"),
-    #     'HOST': config("HOST"),
-    #     'PORT': config("PORT"),
-    # },
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_NAME'),
-        'USER': os.environ.get('POSTGRES_USER'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': 'db',
-        'PORT': 5432,
-    }
+        'ENGINE': config("ENGINE"),
+        'NAME': config("NAME"),
+        'USER': config("USER"),
+        'PASSWORD': config("PASSWORD"),
+        'HOST': config("HOST"),
+        'PORT': config("PORT"),
+    },
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': os.environ.get('POSTGRES_NAME'),
+    #     'USER': os.environ.get('POSTGRES_USER'),
+    #     'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+    #     'HOST': 'db',
+    #     'PORT': 5432,
+    # }
     
 }
 
