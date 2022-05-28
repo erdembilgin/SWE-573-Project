39c03f5e803961fc7cf8a413fd4605e50b4cf57e
erdembilgin <hasanerdembilgin@gmail.com>
2022-05-28 13:01:39 +0500

    a unit test added

--- spaces/tests.py
+++ spaces/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+
+from django.test import TestCase, Client
+from django.urls import reverse
+import json
+from accounts.models import User
+
+
+class TestViews(TestCase):
+     def setUp(self):
+        print("set up is gonna execute")
+        self.client=Client()
+        self.user = User.objects.create_user(email='testuser@mail.com',password='123456')
+        self.user.set_password("123456")
+        self.user.save()
+
+     def test_spaces_homepage_GET(self):
+       self.client.login(email='testuser@mail.com', password='123456')
+       response=self.client.get(reverse('homepage'))
+       self.assertEquals(response.status_code, 200)
+       self.assertTemplateUsed(response,"spaces/homepage.html")
+       
